2f04943886d2af4c4ea02d2089913732314ebd01
kethe sai nikhil <nikhilkethe007@gmail.com>
2024-06-09 16:25:25 +0530

    major update

--- src/app/lib/auth.js
+++ src/app/lib/auth.js
@@ -4,7 +4,6 @@ import GoogleProvider from "next-auth/providers/google";
 const scopes = [
   "https://www.googleapis.com/auth/gmail.readonly",
   "https://www.googleapis.com/auth/userinfo.email",
-  "https://www.googleapis.com/auth/gmail.send",
 ];
 
 export const authOptions = {
